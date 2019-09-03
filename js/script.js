09baa6e7b3b4bbd370ac28946aa1cd91b8f3cc7e
Tatsuya Ichikawa <>
2019-09-04 01:09:59 +0900

    [Add] Add opening anime

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// // titleのfadeinアニメーション
+// let showTitle = function () {
+//   let title = document.getElementById('title')
+//   title.classList.add('title-active');
+// }
+
+// // self-descriptionアニメーション
+// let showDescription = function () {
+//   let desc = document.getElementById('self-description')
+//   desc.classList.add('desc-active');
+// }
+
+// window.onload = function() {
+//   console.log('bbbb')
+//   setTimeout(showDescription, 2500)
+//   setTimeout(showTitle, 3000)
+// }
